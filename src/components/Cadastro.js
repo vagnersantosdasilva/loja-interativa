8626fbffe13f0c3359988092c169f7de2a0e68c6
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2021-06-03 11:55:20 -0300

    Listando produtos existentes

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,15 +1,81 @@
 import ProductForm from "./ProductForm";
+import {useState} from "react";
+import ProductList from "./PorductList";
+import {useProduct} from "../hooks/useProduct";
 
 const Cadastro = ()=>{
+
+    const [product,setProduct] = useState( {id:0,nome:"",fabricante:[],categoria:[],quantidade:0});
+    const service = useProduct();
+    const list = service.list()
+
+    const onSubmitHandler = (event)=>{
+        alert('teste');
+
+    }
+    const onInputChangeHandler = ()=>{
+
+    }
     return (
         <div className="container">
             <div className="row">
                 <div className="col">
-                    <ProductForm></ProductForm>
+                    <div >
+                        <h4>Cadastro de Produtos</h4>
+                        <form >
+                            <div className="form-group">
+                                <label htmlFor="email"><strong>Nome do produto</strong></label>
+                                <input type="nome" className="form-control" name="nome"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="fabricante">Fabricante</label>
+                                <select name="cars" id="fabricante"  name = "fabricante" className="form-control ">
+                                    <option value="f1">Fabricante 1</option>
+                                    <option value="f2">Fabricante 2</option>
+                                    <option value="f3">Fabricante 3</option>
+                                    <option value="f4">Audi</option>
+                                </select>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="categoria">Categoria</label>
+                                <select name="cars" id="fabricante"  name = "fabricante" className="form-control ">
+                                    <option value="c1">Categoria 1</option>
+                                    <option value="c2">Categoria 2</option>
+                                    <option value="c3">Categoria 3</option>
+                                    <option value="c4">Categoria</option>
+                                </select>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="quantidade"><strong>Quantidade</strong></label>
+                                <input type="text" name="quantidade" className="form-control" />
+
+                            </div>
+                            <button  className="btn btn-info" onClick={onSubmitHandler}>Adicionar Produto</button>
+                        </form>
+
+                    </div>
                 </div>
 
                 <div className="col">
                      <h4>Listagem</h4>
+                    <div>
+                        {list.map(i =>
+
+                            <div className="row">
+                                <div className="col">{i.name}</div>
+                                <div className="col">{i.fabricante.name}</div>
+                                <div className="col">{i.fabricante.categoria}</div>
+                                <div className="col">{i.quantidade}</div>
+                                <div className="col">{i.preco}</div>
+                                <div className="col">Editar</div>
+                                <div className="col">Deletar</div>
+
+                            </div>
+                        )}
+                    </div>
                 </div>
 
             </div>
--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -1,8 +1,35 @@
 
 export  const useProduct = ()=>{
 
+    let products = [
+        {   id:1,
+            name:"Product1",
+            fabricante: { name:"f1",categoria:"c1"},
+            quantidade:10,
+            preco:"R$10"
+        },
+        {   id:2,
+            name:"Product2",
+            fabricante: { name:"f1",categoria:"c1"},
+            quantidade:10,
+            preco:"R$10"
+        },
+        {   id:3,
+            name:"Product3",
+            fabricante: { name:"f1",categoria:"c1"},
+            quantidade:10,
+            preco:"R$10"
+        }
+    ]
 
+    const add = (product)=>{
 
-    return {};
+    }
+
+    const list = () =>{
+        return products;
+    }
+
+    return {list,add};
 }
 
